fe3ad504e7d47c46bedbec09953ba7217c9d749b
zoomspeed77 <zoomspeed77@gmail.com>
2020-01-14 00:42:45 +0900

    Add: port 번호 dotenv 적용 기존 포트번호 문자열 값으로 typescript에서 에러남 --> Number로 숫자 처리 함

--- Backend/src/ormConfig.ts
+++ Backend/src/ormConfig.ts
@@ -1,19 +1,19 @@
-import { ConnectionOptions, createConnection, Connection } from 'typeorm';
+import { ConnectionOptions, createConnection, Connection } from "typeorm";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const connectionOptions: ConnectionOptions = {
-	type: 'postgres',
-	database: 'slack',
-	synchronize: true,
-	logging: true,
-	entities: [ 'entities/**/*.*' ],
-	host: process.env.DB_HOST || 'default',
-	port: 5432,
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD
+  type: "postgres",
+  database: "slack",
+  synchronize: true,
+  logging: true,
+  entities: ["entities/**/*.*"],
+  host: process.env.DB_HOST || "default",
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
 };
 
 const connection: Promise<Connection> = createConnection(connectionOptions);
